3e63d734e875f301be0313bfbd17ac95a22d7692
Aleksander Kołata <olek.tbg.konta@gmail.com>
2021-04-05 16:20:19 +0200

    [MO-004] initial project configured

---
+++ src/main/java/pl/sg/meetingorganiser/meetingorganiserservice/api/CreateMeetingRequest.java
@@ -0,0 +1,11 @@
+package pl.sg.meetingorganiser.meetingorganiserservice.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+public class CreateMeetingRequest implements Serializable {
+}
